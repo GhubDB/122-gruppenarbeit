653d30f0286246628f9d3d5109b8d42d56aaeeb2
mb8144 <maurusbrunnschweiler@gmail.com>
2023-12-01 17:08:31 +0100

    before pushing

--- src/database/connect.py
+++ src/database/connect.py
@@ -1,7 +1,3 @@
 from sqlalchemy import create_engine, text
 
 engine = create_engine("sqlite:///database.sqlite3", echo=True)
-
-with engine.connect() as connection:
-  result = connection.execute(text('select "Hello"'))
-  print(result.all())
--- src/database/selector.py
+++ src/database/selector.py
@@ -1,3 +1,8 @@
 from persisting import session
 from models import Date, Time_Entry
+from sqlalchemy import select
 
+dates = session.query(Date).all()
+
+for date in dates:
+  print(date)
